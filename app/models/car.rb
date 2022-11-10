70528b4afb6e950090f979a1ec2e17dc7048770c
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-11-10 10:17:04 +0200

    Add validations for the car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,6 @@
 class Car < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true, length: { in: 0..100 }
+  validates :description, presence: true, length: { in: 0..350 }
+  validates :price, presence: true, numerically: { only_integer: true, greater_than_or_equal_to: 0 }
 end
