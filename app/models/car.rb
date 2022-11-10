7a4303607f5825f98987532f95b4c4f3472ac944
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-11-10 16:14:10 +0200

    Add tests for car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,7 +4,7 @@ class Car < ApplicationRecord
   validates :image, presence: true, length: { in: 0..250 }
   validates :name, presence: true, length: { in: 0..100 }
   validates :description, presence: true, length: { in: 0..350 }
-  validates :price, presence: true, numerically: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { only_decimal: true, greater_than_or_equal_to: 0 }
   validates :location, presence: true, length: { in: 0..250 }
-  validates :duration, presence: true, numerically: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :duration, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
---
+++ spec/models/car_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Car, type: :model do
+  before(:each) do
+    @car = Car.create(
+      user: @user,
+      name: 'vox wagen',
+      image: 'https://google.com/cars',
+      description: 'This is a brand new v6 vox wagen with great speed',
+      price: 100.0,
+      location: 'kigali',
+      duration: 2,
+      rented: false
+    )
+  end
+
+  context 'invalid inputs' do
+    it 'is inavlid when name is blank' do
+      @car.name = ''
+      expect(@car).to_not be_valid
+    end
+
+    it 'is inavlid when image is blank' do
+      @car.image = ''
+      expect(@car).to_not be_valid
+    end
+
+    it 'is invalid when :description length > 350' do
+      @car.description = 'g' * 351
+      expect(@car).to_not be_valid
+    end
+
+    it 'is invalid when :location length > 250' do
+      @car.location = 'k' * 251
+      expect(@car).to_not be_valid
+    end
+
+    it 'price should be greater than or equal to zero' do
+      @car.price = 0
+      expect(@car).to_not be_valid
+    end
+
+    it 'duration should be greater than or equal to zero' do
+      @car.duration = 0
+      expect(@car).to_not be_valid
+    end
+ end
+end
