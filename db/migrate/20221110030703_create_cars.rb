da0fab1cc3bd4e781f9c51029de3c8e121f4cf68
Favourezeugwa <favourezeugwa@gmail.com>
2022-11-10 04:44:51 +0100

    Add cars migration and schema

---
+++ db/migrate/20221110030703_create_cars.rb
@@ -0,0 +1,16 @@
+class CreateCars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cars do |t|
+      t.string :name
+      t.string :image
+      t.text :description
+      t.decimal :price
+      t.string :location
+      t.integer :duration
+      t.boolean :rented, default: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
