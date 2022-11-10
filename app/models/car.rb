46ff327624ea35f58dcfceee5e95bb1343d5a323
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-11-10 10:24:10 +0200

    Add validation for image

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,10 @@
 class Car < ApplicationRecord
   belongs_to :user
+
+  validates :image, presence: true, length: { in: 0..250 }
   validates :name, presence: true, length: { in: 0..100 }
   validates :description, presence: true, length: { in: 0..350 }
   validates :price, presence: true, numerically: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :location, presence: true, length: { in: 0..250 }
+  validates :duration, presence: true, numerically: { only_integer: true, greater_than_or_equal_to: 0 }
 end
