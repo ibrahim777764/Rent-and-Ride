c341b41917ef63b35d621e669b1bfbb64edf75ef
Favourezeugwa <favourezeugwa@gmail.com>
2022-11-12 22:19:11 +0100

    Add devise gem and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :models
   namespace :api do
     namespace :v1 do
       resources :cars
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_10_030703) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_12_211055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,10 +22,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_030703) do
     t.string "location"
     t.integer "duration"
     t.boolean "rented", default: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
