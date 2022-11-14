b41f2d66e09260975201047fbfc4c4f063701f37
Favourezeugwa <favourezeugwa@gmail.com>
2022-11-14 05:07:59 +0100

    Add users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cars
+      resources :users
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
