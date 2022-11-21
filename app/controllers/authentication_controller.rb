0d184d337475b9935b43b00d24bcfc6a360030a4
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-11-22 00:13:49 +0300

    Fix bugs

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,11 +4,11 @@ class AuthenticationController < ApplicationController
   # POST /auth/login
   def login
     @user = User.find_by_email(params[:email])
-    if @user&.valid_password?(params[:password])
+    if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
-      render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
-                     username: @user.name }, status: :ok
+      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                     username: @user.username }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
