8f963113d7818c42ffb86af1d8b7085aab014b22
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-11-23 17:27:05 +0300

    fix cors bug 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-  origins 'https://protected-sea-38971.herokuapp.com/api/v1/'
-  
+  origins 'https://protected-sea-38971.herokuapp.com'
+
   resource '*',
   headers: :any,
   methods: [:get, :post, :put, :patch, :delete, :options, :head],
