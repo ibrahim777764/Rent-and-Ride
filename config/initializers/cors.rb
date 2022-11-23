7717e075b1bc7a8f090a2172254f705d54490111
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-11-23 16:06:20 +0300

    Change the origins of url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-  origins 'http://localhost:3000'
+  origins 'https://protected-sea-38971.herokuapp.com'
   
   resource '*',
   headers: :any,
