7dbd3c4ec6496b4ebe72a6ad2970d16a77fa9e33
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-11-23 16:50:07 +0200

    update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-  origins 'https://protected-sea-38971.herokuapp.com'
+    origins '*'
 
   resource '*',
   headers: :any,
