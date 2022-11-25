a7ddec163e63644ea8ee06d51d98c90dca3279f5
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-11-25 07:46:53 +0500

    includes association for bookings

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,4 +5,6 @@ class Car < ApplicationRecord
   validates :price, presence: true, numericality: { only_decimal: true, greater_than_or_equal_to: 0 }
   validates :location, presence: true, length: { in: 0..250 }
   validates :duration, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  has_many :bookings
+  has_many :users, through: :bookings
 end
