329ed3f222575f3e01c4000309d3ed3495acc337
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-11-25 07:41:44 +0500

    Add api controller and authorization

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,22 @@
+class ApiController < ActionController::API
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+    def authorize_request
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+            @decoded = AuthenticationTokenService.decode(header)
+            @current_user = User.find(@decoded[:user_id])
+        rescue ActiveRecord::RecordNotFound => e
+            render json: { errors: e.message }, status: :unauthorized
+        rescue JWT::DecodeError => e
+            render json: { errors: e.message }, status: :unauthorized
+        end
+    end
+    def record_not_found(*)
+        render json: { error: error.message }, status: :not_found
+    end
+
+    def current_user
+        @current_user
+    end
+end
