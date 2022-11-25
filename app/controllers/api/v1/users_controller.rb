bdd4ad6cf530c4fe9ec71083e5d7f80079b4048d
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-11-25 07:38:59 +0500

    Modify create user method and user params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,4 +6,31 @@ before_action :authorize_request, except: [:create]
           users: users
       }
   end
+
+  def create
+    user = User.create(user_params)
+    # byebug
+    if user.valid?
+        payload = {user_id: user.id}
+        token = AuthenticationTokenService.encode(payload)
+        render json: {
+            status: :created,
+            user: user,
+            jwt: token,
+            current_user: true
+        }
+    else
+        render json: {
+            status: 500,
+            errors: user.errors.full_messages
+        },
+        status: :not_acceptable
+    end
+end
+
+private
+
+def user_params
+    params.permit(:username, :email, :password, :password_digest)
+end
 end
