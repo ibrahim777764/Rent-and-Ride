db5742b4ec20c6ca60805bf926a9ad5fadb45dfa
brainconnect93 <brainconnect0@gmail.com>
2022-11-25 04:06:46 +0100

    Migrate the data to the schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_200255) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_030355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "city"
+    t.bigint "car_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "name"
@@ -35,4 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_200255) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
 end
