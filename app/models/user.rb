efe217b77220a46502176b9df0c6a87906bf7895
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-11-25 07:48:28 +0500

    create admin role an association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :bookings
+  has_many :cars, through: :bookings
   validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true
-  validates :password,
-            length: { minimum: 6 },
-            if: -> { new_record? || !password.nil? }
+ 
+  def admin?
+    self.role == "admin"
+  end
 end
